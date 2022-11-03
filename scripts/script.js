07ceb24067d5b33787a98100e732d8f7a1f466c6
nigamajid <74210017+nigamajid@users.noreply.github.com>
2022-11-03 17:48:46 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("hi");
+
+var openButton = document.querySelector("header > button");
+
+openButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+
+  var deNav = document.querySelector("nav");
+
+  deNav.classList.add("toonMenu");
+}
